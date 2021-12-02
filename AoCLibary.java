37669b21571d4c9b42a0a1a26edfad4394645d68
m.witti <https://svn2.se.jku.at/scm/git/pk-ss20-ploesch-team-03>
2021-12-02 16:58:47 +0100

    day1

--- AoCLibary.java
+++ AoCLibary.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+
 public class AoCLibary {
+
     //Libary class
-    
+    //day1 part1
+    public int getIncreaseLevels(ArrayList<Integer> inputList) {
+        int count = 0;
+        int prev = inputList.get(0);
+        for(int i : inputList){
+            if(i > prev) {
+                count++;            
+            }
+            prev = i;
+        }
+        return count;
+    }
+
+    //day1 part2
+    public int getIncreaseLevelsOf3(ArrayList<Integer> inputList) {
+        int count = -1;
+        int prev = 0;
+        for(int i = 0; i<inputList.size(); i++) {
+            int sum = 0;
+            for(int j = i; j < inputList.size() && j < i+3; j++) {
+                sum += inputList.get(j);               
+            }
+            if(prev<sum) {
+                count++;
+            }
+            prev = sum;
+        }
+        return count;
+    }
 }
--- AoCMain.java
+++ AoCMain.java
@@ -1,8 +1,25 @@
-public class AoCMain {
-    AoCLibary aoCLibary = new AoCLibary();
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
+public class AoCMain {
+
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        AoCLibary aoCLibary = new AoCLibary();
+        try(BufferedReader br = new BufferedReader(new FileReader("resources\\day1input2.txt"))) {
+            ArrayList<Integer>integers = new ArrayList<>();
+            String line = br.readLine();
         
+            while (line != null) {
+                integers.add(Integer.parseInt(line));
+                line = br.readLine();
+            }
+            System.out.println(aoCLibary.getIncreaseLevels(integers));
+            System.out.println(aoCLibary.getIncreaseLevelsOf3(integers));
+        }
     }
     
 }

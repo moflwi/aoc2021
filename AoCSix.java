31b25f22a81e4d4dd5bb8003f13d178b243c8026
m.witti <https://svn2.se.jku.at/scm/git/pk-ss20-ploesch-team-03>
2021-12-06 13:53:28 +0100

    adapted to not go brrrr

--- AoCSix.java
+++ AoCSix.java
@@ -2,37 +2,32 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AoCSix {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        try(BufferedReader br = new BufferedReader(new FileReader("resources\\day6input.txt"))) {         
-           
-    
+        try(BufferedReader br = new BufferedReader(new FileReader("resources\\day6input.txt"))) {             
             String line = br.readLine();
-            ArrayList<LanternFish> list = new ArrayList<>();
-            
-    
+            long[]count = new long[9]; 
             while (line != null) {
                 String[] s = line.split(",");
                 for(String str : s) 
-                    list.add(new LanternFish(Integer.parseInt(str)));                     
+                    count[Integer.parseInt(str)]++;                    
                 line = br.readLine();
             }
-            System.out.println(list.size());
-            for(int i = 31; i >= 0; i--) {
-                ArrayList<LanternFish> tempList = new ArrayList<>();
-                for(LanternFish lf : list) {
-                    LanternFish lanternFish = lf.decrement();
-                    if(lanternFish!=null)
-                        tempList.add(lanternFish);
+            long fish = 0;
+            for(int i = 0; i < 256; i++) {
+                if(count[0]>0)
+                    fish = count[0];
+                for(int j = 1; j < count.length; j++) {
+                    count[j-1] = count[j];
+                    count[j] = 0;
                 }
-                list.addAll(tempList);
-                tempList.clear();
-
+                count[8]+=fish;
+                count[6]+=fish;
+                fish = 0;
             }
-            System.out.println(list.size());
-
+            System.out.println(Arrays.stream(count).sum());
 
         }
 
